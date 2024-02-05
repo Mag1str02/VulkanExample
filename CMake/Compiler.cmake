8227076bad5947c9defc8c4aae86658e4e64e4a0
Mag1str0 <rysin_d@mail.ru>
2024-02-05 17:15:21 +0300

    Compiler cmake

---
+++ CMake/Compiler.cmake
@@ -0,0 +1,17 @@
+set(CMAKE_CXX_STANDARD 23)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+message(STATUS "Compiler: ${CMAKE_CXX_COMPILER_ID}")
+message(STATUS "Compiler: Sanitizers are ${SANITIZERS_ENABLED}")
+
+set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -fPIC -fno-omit-frame-pointer")
+set(CMAKE_C_FLAGS  "${CMAKE_C_FLAGS} -fPIC -fno-omit-frame-pointer")
+
+if (${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+    message(STATUS "Compiler: Using libc++")
+    set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+    if (${SANITIZERS_ENABLED})
+        add_compile_options(-fsanitize=address,undefined)
+        add_link_options(-fsanitize=address,undefined)
+    endif()
+endif()
